bc7980e6d722e2e0d8482ef846e16c16a17e137c
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2020-12-22 00:55:03 -0600

    Build dynamicnav and add css

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log("Test Link");
+
+document.body.onload = newMenu;
+// TESTING
+
+// select the all (sections) in the page
+const mainHeading = document.querySelectorAll('section');
+
+function newMenu() {
+  for (const section of mainHeading) {
+    // Create Elements
+    const listItem = document.createElement('li'); // creat a new li
+    const achor = document.createElement('a'); // creat a new href
+
+    const mainNav = document.getElementById('nav-items') // add li to main ul
+
+    //Getting Data
+    const data = section.dataset.nav; // get the menu name
+    const idName = section.id; // get section id value
+
+    // Setting A Link Data
+    achor.textContent = data; // Giving Href Name Value
+    achor.setAttribute('href', `#${idName}`); // Giving Href Link Value
+
+    listItem.appendChild(achor); // appending href to li
+    mainNav.appendChild(listItem); // appending li to ul
+  }
+}
